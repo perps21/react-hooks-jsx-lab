417b3f4451a52edddc74f347b689d575125b1634
perps21 <wise.abonyo@gmail.com>
2023-07-19 16:43:15 +0300

    completed assingment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>I am Passionate about coding</p>
+ 
+    <img src ="https://i.imgur.com/mV8PQxj.gif" alt=" I made this"/>
+  </div>;
 }
 
 export default About;
