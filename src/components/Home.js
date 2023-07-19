bf71fdf7d88572ea3fde5130a3fa3e54ac09aa51
perps21 <wise.abonyo@gmail.com>
2023-07-19 16:45:39 +0300

    completed assingment

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
   // update the JSX being returned!
  
   return <div id="home">
-    <h1 style={{color:'firebrick'}}>My Name is {name} a Web Developer from {city} </h1>
+    <h1 style={{color:'firebrick'}}> {name} is a Web Developer from {city} </h1>
   </div>;
 }
 
